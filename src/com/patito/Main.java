4ba2d0e73e2213d5bdacc943ed5dcdd4a561e468
Zotblase <fausto.41@hotmail.com>
2024-01-10 18:35:43 -0300

    Agregada la librería de Jackson y sus dependencias

--- src/com/patito/Main.java
+++ src/com/patito/Main.java
@@ -1,9 +1,12 @@
 package com.patito;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 public class Main {
     public static void main(String[] args) {
         UI iniciarVentana = new UI();
         iniciarVentana.show();
 
+        ObjectMapper
     }
 }
