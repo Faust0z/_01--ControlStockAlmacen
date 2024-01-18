d6e926ab3d2817089b7355e39f1a50a26eade4c1
Juan Zarate <juan.zarate497@gmail.com>
2024-01-18 15:00:55 -0300

    Se encontraron los siguientes errores dentro del programa:
    *La clase Scanner y ParseException no se encontraban importadas.
    
    *Para ingresar las fechas en "ultimaVenta y ultimoIngreso" se estaba pasando por parametro el objeto Scanner en vez del nombre del mismo.
    
    *En el procedimiento "agregarProducto" a la hora de pasar todos los atributos habia un error de tipeo, queriendo pasar los atributos con el tipo de dato, cuando este ultimo no es necesario.
    
    *Error de tipeo en "ListaProductos.size()" estaba puesto "ListaProductos.zide".
    
    *Errores de tipeo sobre los "ListaProductos.get(i).AlgunAtributo", estaba tipeado de la siguiente forma "ListaProductos[i].AlgunAtributo".
    
    Fecha 18/1/23.

--- src/com/canard/Productos.java
+++ src/com/canard/Productos.java
@@ -1,5 +1,7 @@
 package com.canard;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.ArrayList;
 import java.util.Scanner;
@@ -14,9 +16,10 @@ public class Productos {
     String descripcion;
 
     ArrayList<Productos> ListaProductos = new ArrayList<Productos>(); // creo la lista
+    Scanner sc = new Scanner(System.in);
 
     public Productos(int ID_producto, String nombre, int stock, int precioUnitario, Date ultimaVenta, Date ultimoIngreso, String descripcion) { //metodo constructor
-        Scanner sc = new Scanner(System.in);
+
 
         System.out.println("ingrese el id del producto: ");
         this.ID_producto = sc.nextInt();
@@ -34,17 +37,15 @@ public class Productos {
         sc.nextLine(); //come el enter
 
         System.out.println("ingrese la fecha de la ultima venta (Formato: dd/MM/yyyy) : ");
-        this.ultimaVenta = ingresarFecha(Scanner);
+        this.ultimaVenta = ingresarFecha(sc);
 
         System.out.println("ingrese la fecha del ultimo ingreso (Formato: dd/MM/yyyy) : ");
-        this.ultimoIngreso = ingresarFecha(Scanner);
+        this.ultimoIngreso = ingresarFecha(sc);
 
         this.descripcion = sc.nextLine();
 
     }
 
-
-    }
 
     public Date ingresarFecha(Scanner sc){ // convierte la fecha ingresada en tipo string y la transforma en tipo date
 
@@ -60,56 +61,56 @@ public class Productos {
 
     }
     public void agregarProducto(){ //agrega el producto a la lista con sus atributos mediante el metodo constructor
-        ListaProductos.add(new Productos(int ID_producto, String nombre, int stock, int precioUnitario, Date ultimaVenta, Date ultimoIngreso, String descripcion));
+        ListaProductos.add(new Productos(ID_producto, nombre, stock, precioUnitario, ultimaVenta, ultimoIngreso, descripcion));
     }
 
+//int ID_producto, String nombre, int stock, int precioUnitario, Date ultimaVenta, Date ultimoIngreso, String descripcion
 
 
-
-    public void incrementarStock(Scanner sc){ //incrementa el stock las veces que ingreses por teclado// tilizando el id
+    public void incrementarStock(Scanner sc){ //incrementa el stock las veces que ingreses por teclado// utilizando el id
         System.out.println("ingrese el id que quiere incrementar el stock: ");
-        id_producto_incrementar = sc.nextInt();
-        for(int i = 0; i < ListaProductos.zide; i++)
-            if(id_pruducto_incrementar == ID_producto){
+        int id_producto_incrementar = sc.nextInt();
+        for(int i = 0; i < ListaProductos.size(); i++)
+            if(id_producto_incrementar == ID_producto){
                 System.out.println("cual es la cantidad de productos que queres agregar al stock?: ");
-                ListaProductos[i].stock += sc.nextInt();
+                ListaProductos.get(i).stock += sc.nextInt();
                 break;
             }
     }
 
     public void reducirStock(){  //decrementa en 1 al stock utilizando el id
         System.out.println("ingrese el id que quiere decrementar el stock: ");
-        id_producto_incrementar = sc.nextInt();
-        for(int i = 0; i < ListaProductos.zide; i++)
-            if(id_pruducto_incrementar == ID_producto){
+        int id_producto_incrementar = sc.nextInt();
+        for(int i = 0; i < ListaProductos.size(); i++)
+            if(id_producto_incrementar == ID_producto){
                 System.out.println("cual es la cantidad de productos que queres quitar al stock?: ");
-                ListaProductos[i].stock -= sc.nextInt();
+                ListaProductos.get(i).stock -= sc.nextInt();
                 break;
             }
     }
 
     public void consultarStock(){  //te da la información del stock para 1 producto en especifico utilizando su id
         System.out.println("ingrese el id que quiere consultar el stock: ");
-        id_producto_incrementar = sc.nextInt();
-        for(int i = 0; i < ListaProductos.zide; i++)
-            if(id_pruducto_incrementar == ID_producto){
-                System.out.println("el stock del producto es: " + ListaProductos[i].stock);
+        int id_producto_incrementar = sc.nextInt();
+        for(int i = 0; i < ListaProductos.size(); i++)
+            if(id_producto_incrementar == ID_producto){
+                System.out.println("el stock del producto es: " + ListaProductos.get(i).stock);
                 break;
             }
     }
 
     public void consultarDatos(){  //consulta todos los datos de un producto en especifico utilizando su id
         System.out.println("ingrese el id que quiere consultar sus datos: ");
-        id_producto_incrementar = sc.nextInt();
-        for(int i = 0; i < ListaProductos.zide; i++)
-            if(id_pruducto_incrementar == ID_producto){
-                System.out.println("ID: " + ListaProductos[i].ID_producto);
-                System.out.println("Nombre: " + ListaProductos[i].nombre);
-                System.out.println("Stock: " + ListaProductos[i].stock);
-                System.out.println("Precio Unitario: " + ListaProductos[i].precioUnitario);
-                System.out.println("Última Venta: " + ListaProductos[i].ultimaVenta);
-                System.out.println("Último Ingreso: " + ListaProductos[i].ultimoIngreso);
-                System.out.println("Descripción: " + ListaProductos[i].descripcion);
+        int id_producto_incrementar = sc.nextInt();
+        for(int i = 0; i < ListaProductos.size(); i++)
+            if(id_producto_incrementar == ID_producto){
+                System.out.println("ID: " + ListaProductos.get(i).ID_producto);
+                System.out.println("Nombre: " + ListaProductos.get(i).nombre);
+                System.out.println("Stock: " + ListaProductos.get(i).stock);
+                System.out.println("Precio Unitario: " + ListaProductos.get(i).precioUnitario);
+                System.out.println("Última Venta: " + ListaProductos.get(i).ultimaVenta);
+                System.out.println("Último Ingreso: " + ListaProductos.get(i).ultimoIngreso);
+                System.out.println("Descripción: " + ListaProductos.get(i).descripcion);
                 break;
             }
     }
